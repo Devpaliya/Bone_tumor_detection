62ea3323901903ad394b628fa6f07e84c2f0b762
Devpaliya <paliyadev82@gmail.com>
2026-04-11 18:55:49 +0530

    frontend ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ function App() {
 
   const handleSubmit = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://bone-tumor-detection-vj6v.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
